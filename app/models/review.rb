9d48a0a43791bd1773e6832cad590dcdb300cccf
Andrew Dembiczak <andrew.dembiczak@gmail.com>
2023-05-10 13:53:43 +0900

    Validations implemented

---
+++ app/models/review.rb
@@ -0,0 +1,10 @@
+class Review < ApplicationRecord
+  belongs_to :restaurant
+  validates :content, presence: true
+  validates :rating, presence: true
+  validates :rating, inclusion: { in: (0..5) }
+  validates :rating, numericality: { only_integer: true }
+end
+
+# A review’s rating must be a number between 0 and 5.
+# A review’s rating must be an integer. For example, a review with a rating of 2.5 should be invalid!
